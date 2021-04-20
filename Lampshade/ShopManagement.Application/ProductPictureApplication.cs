fd922d422db9b4fae21e73c2ea8d5cd7f494b4ef
Alireza Pakravan <66172389+alirezapaakraavaan@users.noreply.github.com>
2021-04-20 14:29:14 +0430

    File Upload Side Effects in UI Solved

--- Lampshade/ShopManagement.Application/ProductPictureApplication.cs
+++ Lampshade/ShopManagement.Application/ProductPictureApplication.cs
@@ -1,26 +1,37 @@
 ﻿using System.Collections.Generic;
 using _0_Framework.Application;
 using ShopManagement.Application.Contracts.ProductPicture;
+using ShopManagement.Domain.ProductAgg;
 using ShopManagement.Domain.ProductPictureAgg;
 
 namespace ShopManagement.Application
 {
     public class ProductPictureApplication : IProductPictureApplication
     {
+        private readonly IFileUploader _fileUploader;
+        private readonly IProductRepository _productRepository;
         private readonly IProductPictureRepository _productPictureRepository;
 
-        public ProductPictureApplication(IProductPictureRepository productPictureRepository)
+        public ProductPictureApplication(IProductPictureRepository productPictureRepository, IProductRepository productRepository,
+            IFileUploader fileUploader)
         {
             _productPictureRepository = productPictureRepository;
+            _productRepository = productRepository;
+            _fileUploader = fileUploader;
         }
 
         public OperationResult Create(CreateProductPicture command)
         {
             var operation = new OperationResult();
-            if (_productPictureRepository.Exists(x => x.Picture == command.Picture && x.ProductId == command.ProductId))
-                operation.Failed(ApplicationMessages.DuplicatedRecord);
 
-            var productPicture = new ProductPicture(command.ProductId, command.Picture, command.PictureAlt,
+            /*if (_productPictureRepository.Exists(x => x.Picture == command.Picture && x.ProductId == command.ProductId))
+            operation.Failed(ApplicationMessages.DuplicatedRecord);*/
+
+            var product = _productRepository.GetProductWithCategory(command.ProductId);
+            var path = $"{product.Category.Slug}//{product.Slug}";
+            var picturePath = _fileUploader.Upload(command.Picture, path);
+
+            var productPicture = new ProductPicture(command.ProductId, picturePath, command.PictureAlt,
                 command.PictureTitle);
             _productPictureRepository.Create(productPicture);
             _productPictureRepository.SaveChanges();
@@ -30,15 +41,14 @@ namespace ShopManagement.Application
         public OperationResult Edit(EditProductPicture command)
         {
             var operation = new OperationResult();
-            var productPicture = _productPictureRepository.Get(command.Id);
+            var productPicture = _productPictureRepository.GetWithProductAndCategory(command.Id);
             if (productPicture == null)
                 return operation.Failed(ApplicationMessages.RecordNotFound);
 
-            if (_productPictureRepository.Exists(x =>
-                x.Picture == command.Picture && x.ProductId == command.ProductId && x.Id != command.Id))
-                operation.Failed(ApplicationMessages.DuplicatedRecord);
+            var path = $"{productPicture.Product.Category.Slug}//{productPicture.Product.Slug}";
+            var picturePath = _fileUploader.Upload(command.Picture, path);
 
-            productPicture.Edit(command.ProductId, command.Picture, command.PictureAlt, command.PictureTitle);
+            productPicture.Edit(command.ProductId, picturePath, command.PictureAlt, command.PictureTitle);
             _productPictureRepository.SaveChanges();
             return operation.Succeeded();
         }
--- Lampshade/ShopManagement.Application/SlideApplication.cs
+++ Lampshade/ShopManagement.Application/SlideApplication.cs
@@ -7,17 +7,22 @@ namespace ShopManagement.Application
 {
     public class SlideApplication : ISlideApplication
     {
+        private readonly IFileUploader _fileUploader;
         private readonly ISlideRepository _slideRepository;
 
-        public SlideApplication(ISlideRepository slideRepository)
+        public SlideApplication(ISlideRepository slideRepository, IFileUploader fileUploader)
         {
             _slideRepository = slideRepository;
+            _fileUploader = fileUploader;
         }
 
         public OperationResult Create(CreateSlide command)
         {
             var operation = new OperationResult();
-            var slide = new Slide(command.Picture, command.PictureAlt, command.PictureTitle, command.Heading,
+
+            var pictureName = _fileUploader.Upload(command.Picture, "slides");
+
+            var slide = new Slide(pictureName, command.PictureAlt, command.PictureTitle, command.Heading,
                 command.Text, command.BtnText, command.Title, command.Link);
 
             _slideRepository.Create(slide);
@@ -28,11 +33,14 @@ namespace ShopManagement.Application
         public OperationResult Edit(EditSlide command)
         {
             var operation = new OperationResult();
+
+            var pictureName = _fileUploader.Upload(command.Picture, "slides");
+
             var slide = _slideRepository.Get(command.Id);
             if (slide == null)
                 return operation.Failed(ApplicationMessages.RecordNotFound);
 
-            slide.Edit(command.Picture, command.PictureAlt, command.PictureTitle, command.Heading, command.Title,
+            slide.Edit(pictureName, command.PictureAlt, command.PictureTitle, command.Heading, command.Title,
                 command.Text, command.BtnText, command.Link);
 
             _slideRepository.SaveChanges();
